9d932cd6f4dbb0875b1c8af5e77fdccd397a4f2f
alexsolex <alexsolex@64680cc8-7926-0410-b946-b1d96f97c161>
2007-01-22 23:01:29 +0000

    création de la classe contextuelle

--- F2XTV_V2/F2XTVgui.py
+++ F2XTV_V2/F2XTVgui.py
@@ -79,13 +79,102 @@ class main(xbmcgui.Window):
         self.close()
         
     def onAction(self,action):
-        if action == 1:
+        if action.getButtonCode() in ACTION_BACK:
             self.close()
+        elif action.getButtonCode() in ACTION_CONTEXT and \
+        self.getFocus()==self.controls[30]:
+        #appui du bouton blanc sur un élément de liste
+            #on récupère l'élément
+            item=self.controls[30].getSelectedPosition()
+            #if self.contexte == "chaines":
+            menucontextuel = contextuel(self.contexte,"France 2")
+            menucontextuel.doModal()
+            RetourControl = menucontextuel.retour
+            del menucontextuel
+            #traitement du retour
+            
 
     def onControl(self,control):
-        if control == self.bouton:
-            pass
+        pass
 
+
+class contextuel(xbmcgui.WindowDialog):
+    """
+    Affichage d'un menu contextuel sur appel dans la liste avec le bouton blanc
+    """
+    def __init__(self,contexte,titre="Menu contextuel"):
+        self.contexte = contexte
+        self.titre = titre
+        #variable qui mémorisera le control pressé (none par défaut)
+        self.retour = None
+        #affichage des controls généraux
+        print "déclaration initiale du fond d'écran"
+        print "déclaration initiale du label pour le titre du menu contextuel : \"%s\""%titre
+        #affichage des boutons du menu contextuel :
+        self.drawContexte(contexte)
+
+    def drawContexte(contexte):
+        if contexte == "chaines":
+            """
+            Menu contextuel de la liste des chaines
+            """
+            print "création des boutons pour le menu contextuel des chaines"
+            print "\t- Regarder"
+            print "\t- Enregistrer"
+            print "\t- Supprimer"
+            print "\t- Gérer la liste des chaines"
+            print "\t- etc..."
+            print "\t- Annuler"
+            print
+            print "Puis liaisons logiques des mouvements entre les controles"
+            print "  (par appel d'une fonction mouvements avec la liste des contrôles en argument)"
+            print
+            print "Pour finir, activation du focus sur le premier bouton"
+            
+        elif contexte == "progs":
+            """
+            Menu contextuel de la liste des programmations
+            """
+            print "création des boutons pour le menu contextuel des programmations"
+            print "\t- Editer"
+            print "\t- Copier"
+            print "\t- Supprimer"
+            print "\t- etc..."
+            print "\t- Nouvelle programmation"
+            print "\t- Annuler"
+            print
+            print "Puis liaisons logiques des mouvements entre les controles"
+            print "  (par appel d'une fonction mouvements avec la liste des contrôles en argument)"
+            print
+            print "Pour finir, activation du focus sur le premier bouton"
+
+        elif contexte == "enreg":
+            """
+            Menu contextuel de la liste des enregistrements
+            """
+            print "création des boutons pour le menu contextuel des enregistrements"
+            print "\t- Regarder"
+            print "\t- Déplacer"
+            print "\t- Supprimer"
+            print "\t- Renommer"
+            print "\t- etc..."
+            print "\t- Annuler"
+            print
+            print "Puis liaisons logiques des mouvements entre les controles"
+            print "  (par appel d'une fonction mouvements avec la liste des contrôles en argument)"
+            print
+            print "Pour finir, activation du focus sur le premier bouton"
+
+    def onAction(self,action):
+        print action
+
+    def onControl(self,control):
+        print control
+        if control == "bouton annuler":
+            self.control = None
+        else:
+            self.retour = control
+        self.close()
 
 
 m = main()
